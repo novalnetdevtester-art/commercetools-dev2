85d1a346a1c4f31d0a35f62d389b439491933e70
novalnetdevtester-art <novalnetdevtester@gmail.com>
2026-08-17 16:26:22 +0530

    Update paypal.ts

--- enabler/src/components/payment-methods/paypal/paypal.ts
+++ enabler/src/components/payment-methods/paypal/paypal.ts
@@ -55,7 +55,7 @@ export class Paypal extends BaseComponent {
     try {
       const requestData: PaymentRequestSchemaDTO = {
         paymentMethod: {
-          type: this.paymentMethod,
+          type: 'PayPal',
         },
         paymentOutcome: PaymentOutcome.AUTHORIZED,
         lang: pathLocale,
